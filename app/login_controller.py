9aa5109e5088f791ad830db8eae069c7887d33e0
Nayana Kumari <70977839+nt27web@users.noreply.github.com>
2020-12-10 00:28:05 -0500

    Task: updated button styles and cleaned up code

--- app/login_controller.py
+++ app/login_controller.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
 
+
 app = Flask(__name__)
 mysql = MySQL(cursorclass=DictCursor)
 
@@ -69,51 +70,6 @@ def sign_up_submit() -> str:
     cursor.execute(sign_up_query, inputData)
     mysql.get_db().commit()
     return render_template('login.html', title='Login')
-
-
-
-
-"""
-
-@app.route('/calendar', methods=['GET'])
-def index():
-    user = {'username': 'Cities Project'}
-    cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblCities')
-    result = cursor.fetchall()
-    return render_template('index.html', title='Home', user=user, cities=result)
-    
-@app.route('/edit/<int:Id>', methods=['POST'])
-def form_update_post(Id):
-    cursor = mysql.get_db().cursor()
-    input_data = (request.form.get('LatD'), request.form.get('LatM'), request.form.get('LatS'), request.form.get('NS')
-                 , request.form.get('LonD'), request.form.get('LonM'), request.form.get('LonS'), request.form.get('EW')
-                 , request.form.get('City'), request.form.get('State'), Id)
-    sql_update_query = 'UPDATE tblCities t 
-                            SET t.LatD = %s, t.LatM = %s, t.LatS = %s, t.NS = %s
-                            , t.LonD = %s, t.LonM = %s, t.LonS = %s, t.EW = %s
-                            , t.City = %s, t.State = %s
-                            WHERE t.Id = %s'
-    cursor.execute(sql_update_query, input_data)
-    mysql.get_db().commit()
-    return redirect("/", code=302)
-
-
-@app.route('/cities/new', methods=['GET'])
-def form_insert_get():
-    return render_template('new.html', title='New City Form')
-
-
-@app.route('/cities/new', methods=['POST'])
-def form_insert_post():
-    cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('LatD'), request.form.get('LatM'), request.form.get('LatS'), request.form.get('NS')
-                 , request.form.get('LonD'), request.form.get('LonM'), request.form.get('LonS'), request.form.get('EW')
-                 , request.form.get('City'), request.form.get('State'))
-    sql_insert_query = 'INSERT INTO tblCities (LatD,LatM,LatS,NS,LonD,LonM,LonS,EW,City,State) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) '
-    cursor.execute(sql_insert_query, inputData)
-    mysql.get_db().commit()
-    return redirect("/", code=302)"""
 
 
 if __name__ == '__main__':
