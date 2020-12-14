05e56a79e6d6529504b48972bac4908d53068e20
Nayana Kumari <70977839+nt27web@users.noreply.github.com>
2020-12-14 16:05:19 -0500

    feature: email validation api integrated

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ WORKDIR /app
 
 RUN pip install --upgrade pip
 
-copy requirements.txt /app
+copy requirements.txt .
 
 RUN pip install -r requirements.txt
 
--- app/login_controller.py
+++ app/login_controller.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, json
-from flask import render_template
+from flask import Flask, request, json, make_response, render_template
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
+from flask_email_verifier import EmailVerifier
+from json import dumps, loads
+import os
 
 
 app = Flask(__name__)
@@ -9,10 +11,13 @@ mysql = MySQL(cursorclass=DictCursor)
 
 app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root' #os.environ['MYSQL_ROOT_PASSWORD']
 app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_DB'] = 'final_project_db'
 mysql.init_app(app)
+# Initialize the extension
+app.config['EMAIL_VERIFIER_KEY'] = 'at_d2rnczuTQlRMWTq5qElyv5fr4nwYi'
+verifier = EmailVerifier(app)
 
 
 @app.route('/', methods=['GET'])
@@ -70,6 +75,19 @@ def sign_up_submit() -> str:
     cursor.execute(sign_up_query, inputData)
     mysql.get_db().commit()
     return render_template('login.html', title='Login')
+
+
+@app.route('/email/<email>')
+def email(email):
+    # Retrieve an info for the given email address
+    email_address_info = verifier.verify(email)
+    if email_address_info is not None:
+        data = dumps(loads(email_address_info.json_string), indent=4)
+        resp = make_response(data, 200)
+        resp.headers['Content-Type'] = 'application/json'
+    else:
+        resp = make_response('None', 404)
+    return resp
 
 
 if __name__ == '__main__':
